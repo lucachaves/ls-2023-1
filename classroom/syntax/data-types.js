20a2e8a0c57a1d5fe6935877000d11dc82ab0b4e
Luiz <lucachaves@gmail.com>
2023-02-13 16:18:01 -0300

    js hello, data type

---
+++ classroom/syntax/data-types.js
@@ -0,0 +1,32 @@
+// Boolean
+console.log(true);
+console.log(false);
+
+// undefined
+console.log(undefined);
+
+// null
+console.log(null);
+
+// Number
+console.log(-15);
+console.log(15);
+console.log(0b1111);
+console.log(0o17);
+console.log(0xf);
+console.log(15.15);
+console.log(15.15e10);
+
+// String
+console.log('ifpb');
+//  console.log("ifpb");
+console.log(`ifpb`);
+console.log(`ifpb
+cstsi`);
+
+// Array
+console.log([1, 2, 3]);
+console.log([1, 'ifpb', true, [1, 2]]);
+
+// JSON
+console.log({ name: 'Luiz', email: 'luiz@email.com' });
