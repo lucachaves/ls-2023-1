341bdafe538342a557f1923a71e36bc0b8b49fb3
Luiz <lucachaves@gmail.com>
2023-02-15 14:06:07 -0300

    sintase básica - schema

---
+++ classroom/syntax/statements.js
@@ -0,0 +1,15 @@
+// statements (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements)
+
+// if
+
+// if..esle
+
+// if..else if..else
+
+// switch
+
+// while
+
+// do..while
+
+// for
---
+++ classroom/syntax/variable.js
@@ -0,0 +1,25 @@
+// without declaration keyword
+
+// const
+
+// 'const' declarations must be initialized.
+
+// TypeError: Assignment to constant variable.
+
+// not imutable
+
+// Cannot redeclare block-scoped variable 'number'
+
+// let
+
+// Cannot redeclare block-scoped variable 'value'
+
+// var
+
+// reassignment
+
+// dynamic types
+
+// case sensitive
+
+// weak type
