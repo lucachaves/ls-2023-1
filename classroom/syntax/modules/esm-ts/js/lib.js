ec9c13b7239a3e216c504cce93267d7438ba11bc
Luiz <lucachaves@gmail.com>
2023-03-01 16:47:51 -0300

    typescript

---
+++ classroom/syntax/modules/esm-ts/js/lib.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.minus = exports.sum = exports.PI = void 0;
+exports.PI = 3.14;
+function sum(a, b) {
+    return a + b;
+}
+exports.sum = sum;
+function minus(a, b) {
+    return a - b;
+}
+exports.minus = minus;
+exports.default = { sum, minus };
---
+++ classroom/syntax/modules/esm-ts/src/main.ts
@@ -0,0 +1,4 @@
+import calc, { sum } from './lib';
+
+console.log(calc.sum(1, 1));
+console.log(sum(1, 1));
