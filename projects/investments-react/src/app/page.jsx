d3c8a5a9d855a2f3ac0b10e9f578722991182243
Luiz <lucachaves@gmail.com>
2023-06-19 12:37:20 -0300

    state, list e form

--- projects/investments-react/src/app/page.jsx
+++ projects/investments-react/src/app/page.jsx
@@ -1,22 +1,207 @@
+'use client';
+
+import { useState } from 'react';
 import InvestmentCard from '@/components/InvestmentCard';
+import { investments as investData } from './data';
+
+const categories = [
+  'Escolha uma categoria',
+  'Pós',
+  'Pré',
+  'IPCA',
+  'Renda Variável',
+  'Alternativos',
+];
 
 export default function Home() {
-  const investment = [
-    {
-      name: 'Tesouro IPCA+ 2035',
-    },
-    {
-      name: 'Tesouro IPCA+ 2045',
-    },
-  ];
+  const [investments, setInvestments] = useState(investData);
+
+  const handleClick = () => {
+    const newInvestment = {
+      name: 'IVVB11',
+      value: 47.76,
+      origin: 'BlackRock',
+      category: 'Renda Variável',
+      date: '2023-06-19',
+    };
+
+    setInvestments((previous) => [...previous, newInvestment]);
+  };
 
   return (
-    <div className="container mx-auto lg:max-w-screen-lg">
-      <h1 className="text-center text-2xl my-12 font-bold">Investimentos</h1>
-      <div className="investments grid grid-cols-3 gap-3">
-        <InvestmentCard name={investment[0].name} value={100} />
-        <InvestmentCard name={investment[1].name} value={200} />
+    <>
+      <div className="container mx-auto lg:max-w-screen-lg">
+        <h1 className="text-center text-2xl my-12 font-bold">Investimentos</h1>
+        <div className="investments grid grid-cols-3 gap-3">
+          {investments.map((investment, index) => (
+            <InvestmentCard investment={investment} key={index} />
+          ))}
+        </div>
+        <div className="fixed bottom-8 right-8">
+          <button
+            className="mr-1.5 inline-block rounded bg-gray-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-gray-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-gray-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-gray-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+            type="button"
+            onClick={handleClick}
+          >
+            +
+          </button>
+        </div>
       </div>
-    </div>
+      <div
+        className="fixed bottom-0 right-0 top-0 z-[1045] flex w-96 max-w-full  flex-col border-none bg-white bg-clip-padding text-neutral-700 shadow-sm outline-none transition duration-300 ease-in-out dark:bg-neutral-800 dark:text-neutral-200 [&[data-te-offcanvas-show]]:transform-none"
+        tabIndex={-1}
+      >
+        <div className="flex items-center justify-between p-4">
+          <h5
+            className="mb-0 font-semibold leading-normal"
+            id="offcanvasRightLabel"
+          >
+            Cadastro de Investimento
+          </h5>
+          <button
+            type="button"
+            className="box-content rounded-none border-none opacity-50 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+            data-te-offcanvas-dismiss=""
+          >
+            <span className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+        <div className="offcanvas-body flex-grow overflow-y-auto p-4">
+          <form className="space-y-4">
+            {/* Name Input */}
+            <div>
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Nome
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Nome do investimento"
+                required
+              />
+            </div>
+
+            {/* Value Input */}
+            <div>
+              <label
+                htmlFor="value"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Valor
+              </label>
+              <input
+                type="text"
+                id="value"
+                name="value"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="100,00"
+                required
+              />
+            </div>
+
+            {/* Origin Input */}
+            <div>
+              <label
+                htmlFor="origin"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Origem
+              </label>
+              <input
+                type="text"
+                id="origin"
+                name="origin"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Tesouro Nacional"
+                required
+              />
+            </div>
+
+            {/* Interest Input */}
+            <div>
+              <label
+                htmlFor="interest"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Taxa
+              </label>
+              <input
+                type="text"
+                id="interest"
+                name="interest"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="100% Selic"
+                required
+              />
+            </div>
+
+            {/* Category Select */}
+            <label
+              htmlFor="category"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Categoria
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              {categories.map((category, index) => (
+                <option value={category} key={index}>
+                  {category}
+                </option>
+              ))}
+            </select>
+
+            {/* Create_at Input */}
+            <div>
+              <label
+                htmlFor="created_at"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Data
+              </label>
+              <input
+                type="date"
+                id="create_at"
+                name="create_at"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="100% Selic"
+                required
+              />
+            </div>
+
+            {/* Submit Input */}
+            <button
+              type="submit"
+              className="dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] inline-block rounded bg-gray-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-gray-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-gray-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-gray-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+            >
+              Cadastrar
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
--- projects/investments-react/src/components/InvestmentCard.jsx
+++ projects/investments-react/src/components/InvestmentCard.jsx
@@ -1,22 +1,28 @@
-export default function InvestmentCard({ name, value }) {
+import { formatCurrency, formatDate } from '@/lib/utils';
+
+export default function InvestmentCard({ investment }) {
   return (
     <div id="investment-1" className="bg-white shadow-md rounded-lg p-4">
       <div className="flex justify-between items-center">
-        <h3 className="text-lg font-semibold text-gray-700">{name}</h3>
-        <p className="text-lg font-semibold text-gray-700">{value}</p>
+        <h3 className="text-lg font-semibold text-gray-700">
+          {investment.name}
+        </h3>
+        <p className="text-lg font-semibold text-gray-700">
+          {formatCurrency(investment.value)}
+        </p>
       </div>
       <div className="mt-4">
         <p className="text-sm text-gray-500">
-          <span className="font-bold">Origem:</span> Tesouro Direto
+          <span className="font-bold">Origem:</span> {investment.origin}
         </p>
         <p className="text-sm text-gray-500">
-          <span className="font-bold">Categoria:</span> IPCA
+          <span className="font-bold">Categoria:</span> {investment.category}
         </p>
         <p className="text-sm text-gray-500">
-          <span className="font-bold">Data:</span> 05/06/2023
+          <span className="font-bold">Data:</span> {formatDate(investment.date)}
         </p>
         <p className="text-sm text-gray-500">
-          <span className="font-bold">Taxa:</span> IPCA + 5,72%
+          <span className="font-bold">Taxa:</span> {investment.interest}
         </p>
       </div>
     </div>
